35440d3424a37fc5cd169d320d365b5edc341228
NatashaKSS <xnagol@gmail.com>
2015-07-19 02:29:12 +0800

    Added better UI for notes & toolbar. Debug instance where draggable would teleport. Debugging swapping notes between sortable lists.

--- jQuery.js
+++ jQuery.js
@@ -1,18 +1,22 @@
 //-----------------------------------------------------------//
 //-------------------Sound Generation------------------------//
 //-----------------------------------------------------------//
-var notes = ["C0","C#0","D0","D#0","E0","F0","F#0","G0","G#0","A0","A#0","B0",
-             "C0","C#0","D0","D#0","E0","F0","F#0","G0","G#0","A0","A#0","B0",
-             "C1","C#1","D1","D#1","E1","F1","F#1","G1","G#1","A1","A#1","B1",
-             "C2","C#2","D2","D#2","E2","F2","F#2","G2","G#2","A2","A#2","B2",
+/*
+ * "C0","C#0","D0","D#0","E0","F0","F#0","G0","G#0","A0","A#0","B0",
+   "C0","C#0","D0","D#0","E0","F0","F#0","G0","G#0","A0","A#0","B0",
+   "C1","C#1","D1","D#1","E1","F1","F#1","G1","G#1","A1","A#1","B1",*/
+
+/*
+ * "C8","C#8","D8","D#8","E8","F8","F#8","G8","G#8","A8","A#8","B8",
+   "C9","C#9","D9","D#9","E9","F9","F#9","G9","G#9","A9","A#9","B9",
+   "C10","C#10","D10","D#10","E10","F10","F#10","G10"*/
+
+var notes = ["C2","C#2","D2","D#2","E2","F2","F#2","G2","G#2","A2","A#2","B2",
              "C3","C#3","D3","D#3","E3","F3","F#3","G3","G#3","A3","A#3","B3",
              "C4","C#4","D4","D#4","E4","F4","F#4","G4","G#4","A4","A#4","B4",
              "C5","C#5","D5","D#5","E5","F5","F#5","G5","G#5","A5","A#5","B5",
              "C6","C#6","D6","D#6","E6","F6","F#6","G6","G#6","A6","A#6","B6",
-             "C7","C#7","D7","D#7","E7","F7","F#7","G7","G#7","A7","A#7","B7",
-             "C8","C#8","D8","D#8","E8","F8","F#8","G8","G#8","A8","A#8","B8",
-             "C9","C#9","D9","D#9","E9","F9","F#9","G9","G#9","A9","A#9","B9",
-             "C10","C#10","D10","D#10","E10","F10","F#10","G10"];
+             "C7","C#7","D7","D#7","E7","F7","F#7","G7","G#7","A7","A#7","B7"];
 // 128 notes, of which, the index of a single note in the array (from 12) corresponds to
 // its own MIDI number
 
@@ -362,7 +366,7 @@ function findMaxLength(){
 
 function playAllSequences() {
     console.log("max length = "  + findMaxLength());
-	console.log("start index = "  + startPlayingFrom);
+    	console.log("start index = " + startPlayingFrom);
     //playSequence(0, startPlayingFrom, playUntil);
 	if(findMaxLength() > 1 && playUntil==0){
 	    playUntil = findMaxLength() - 1;
@@ -507,27 +511,6 @@ function generateDivs(numOfDivs, cssClass, text) {
 	return divString;
 }
 
-// Generate notes divs
-function generateNotes() {
-	var allNotesDivs = "";
-	
-	for (noteNum = 24; noteNum < notes.length; noteNum++) {
-		allNotesDivs += generateNotesDivs(noteNum, notes[noteNum]);
-	}
-	
-	$(function() {
-		$(".notes-buttons-holder").append(allNotesDivs);
-	});
-}
-
-function generateNotesDivs(dataNote, noteName) {
-	var result = '<div class="col-md-1" data-note=' + dataNote + '>' + noteName + '</div>';
-
-	return result;
-}
-
-generateNotes();
-
 /*------------------------------------------------*/
 /*--------Document interaction with JQuery--------*/
 /*------------------------------------------------*/
@@ -549,6 +532,26 @@ $(document).ready(function() {
 			
 		});
 		
+		// On drag start for a note, prevent the window from annoyingly
+		// scrolling as user drags note.
+		$(".col-md-1").on("mousedown",function() {
+			$(".notes-buttons-holder").css({
+				"overflow": "hidden"
+			});
+			
+			// On drag stop, revert back to the scrollbar
+			$(this).on("dragstop", function() {
+				$(".notes-buttons-holder").css({
+					"overflow": "auto"
+				});
+			});
+			
+		}).on("mouseup", function() {
+			$(".notes-buttons-holder").css({
+				"overflow": "auto"
+			});
+		});
+		
 		$("#buttons").draggable({ 
 			axis: "y",
 			cursor: "row-resize",
@@ -559,7 +562,6 @@ $(document).ready(function() {
 		$("#all").on("click", function() {
 			playAllSequences();
 		});
-		
 		
 	  /*  
 		//pause is not working properly
@@ -588,7 +590,8 @@ $(document).ready(function() {
 			
 		    piano.stop("playing");
 		    synthGuitar.stop("playing");
-			violin.stop("playing");
+			
+		    violin.stop("playing");
 		    startPlayingFrom = 0;//change back to default
 			playUntil = findMaxLength() - 1;//change back to default
 		    animation.stopAnimation();
@@ -604,6 +607,7 @@ $(document).ready(function() {
 			$(".sortable-system div").remove(); //remove notes from the timeline
 		    enableLooping = false;
 			clearAllSound();
+			
 			animation.stopAnimation();
 			startPlayingFrom = 0;//change back to default
 			playUntil = findMaxLength() - 1;//change back to default
@@ -862,6 +866,7 @@ function setSortable() {
 			},
 			
 			start: function(event, ui) {
+				//console.log("start");
 				
 				var startPosition = ui.item.index(); //original index
 				ui.item.data('startPos', startPosition); //create data called startPos and set it to startPosition
@@ -873,15 +878,12 @@ function setSortable() {
 			
 			// Whenever user has stopped sorting and the DOM element (HTML) has changed
 			update: function(event, ui) {
+				//console.log("update");
 				
 				var startPosition = ui.item.data('startPos');
-				
-				console.log("start position " + ui.item.data('startPos'));
-				
 				var endPosition = ui.item.index();//new position
 				ui.item.data('endPos', endPosition);//update and save item's latest data attribute
 				var grabbedNote = "";
-				console.log("end position " + ui.item.data('endPos'));
 				
 				if (ui.item.attr('data-note') != "silence") { 
 					grabbedNote = new Note(notes[parseInt(ui.item.attr('data-note'))]);
@@ -895,7 +897,7 @@ function setSortable() {
 					// When swapping from left to right
 					composition.getTrack(trackNumInSortable).splice(endPosition + 1, 0, grabbedNote);
 					composition.getTrack(trackNumInSortable).splice(startPosition, 1);
-					
+				
 				} else if (startPosition > endPosition) {
 					// When swapping from right  to left
 					composition.getTrack(trackNumInSortable).splice(endPosition, 0, grabbedNote);
@@ -907,6 +909,7 @@ function setSortable() {
 			
 			// If item is hovering over timeline
 			over: function(event, ui) {
+				//console.log("over");
 				
 				inBox = true;
 				inBeforeStop = false;
@@ -921,6 +924,7 @@ function setSortable() {
 			// If item is dragged outside timeline OR if item is dropped
 			// onto timeline
 			out: function(event, ui) {
+				//console.log("out");
 				
 				inBox = false;
 				
@@ -936,6 +940,7 @@ function setSortable() {
 			// Just before releasing dragging and item is outside timeline
 			beforeStop: function(event, ui) {
 				//console.log("beforeStop");
+				
 				inBeforeStop = true;
 				
 				if (!inBox) {
@@ -949,12 +954,14 @@ function setSortable() {
 			
 			// When timeline receives the user-dragged note
 			receive: function(event, ui) {
+				//console.log("receive");
+				
 				if(ui.item.attr('data-note') != "silence"){
 		      		
 					var insertedNote = new Note(notes[parseInt(ui.item.attr('data-note'))]);
 		      		//console.log(insertedNote.getDuration());
 					composition.addNote(trackNumInSortable, insertedNote);
-					console.log("note " + ui.item.attr('data-note') + "at index " + ui.item.index());
+					
 				} else {
 					
 					var insertedSilence = new Note(ui.item.attr('data-note'));
@@ -962,7 +969,7 @@ function setSortable() {
 					// ui.item.attr('data-note')) is just the string "silence"
 					
 					composition.addNote(trackNumInSortable, insertedSilence);
-					console.log("note " + ui.item.attr('data-note') + "at index " + ui.item.index());
+					
 				}
 				
 				//console.log("end position " + ui.item.data('endPos'));
@@ -1123,7 +1130,46 @@ function setSortable() {
 
 
 
+/*
+ * How to use the below 2 functions for notes div generation:
+ * Uncomment the "console.log(generateNotes);"
+ * Copy n Paste the code from the console on Chrome/Firefox and paste DIRECTLY to the html.
+ * This is to speed up page load ups - it improves performance.
+ * 
+// Generate notes divs
+function generateNotes() {
+	var allNotesDivs = "";
+	var result = "";
+	var octaveClassName = "octave-holder";
+	var numOfOctaves = 6;
+	var numOfNotesInOctave = 12;
+	
+	for (octaveNum = 0; octaveNum < numOfOctaves; octaveNum++) {
+		
+		allNotesDivs = ""; // refresh allNotesDiv for the next octave's notes
+		result += "<div class='" + octaveClassName + "'>";
+		
+		for (noteNum = 0; noteNum < numOfNotesInOctave; noteNum++) {
+			var noteIndex = noteNum + (octaveNum * numOfNotesInOctave);
+			
+			allNotesDivs += generateNotesDivs(noteIndex, notes[noteIndex]);
+		}
+		
+		result += allNotesDivs + "</div>";
+		
+	}
+	
+	return result;
+}
 
+function generateNotesDivs(dataNote, noteName) {
+	var result = '<div class="col-md-1" data-note=' + dataNote + '>' + noteName + '</div>';
+
+	return result;
+}
+
+//console.log(generateNotes());
+ * */
 
 
 /*
